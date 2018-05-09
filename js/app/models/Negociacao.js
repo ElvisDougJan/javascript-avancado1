5ca348bef4c5abb48308b273a1ce2e5965cfff27
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-05-08 21:31:43 -0300

    Adicionando métodos de acesso para simular encapsulamento

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -2,13 +2,26 @@ class Negociacao {
 
   //Definindo parametros que devem ser passados na instancia
   constructor(data, quantidade, valor) {
-    this.data = data
-    this.quantidade = quantidade
-    this.valor = valor
+    this._data = data
+    this._quantidade = quantidade
+    this._valor = valor
   }
 
   //Adicionando método
   calculaVolume () {
     return this.quantidade * this.valor 
+  }
+
+  //Adicionando métodos de acesso para impedir acesso direto dos atributos com _ (underline)
+  getData() {
+    return this._data
+  }
+
+  getQuantidade () {
+    return this._quantidade
+  }
+
+  getValor() {
+    return this._valor
   }
 }
